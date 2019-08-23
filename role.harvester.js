028f2e60f93a6786c3698a6cb8b9cdce0067847b
root <kolymine@gmail.com>
2019-08-23 13:41:52 +0200

    Added memory to harvester to fully empty selves, And to add storage in the valid empty points.

--- role.harvester.js
+++ role.harvester.js
@@ -2,32 +2,37 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES_ACTIVE);
-            if(creep.harvest(creep.pos.findClosestByPath(sources)) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.pos.findClosestByPath(sources));
-            }
-        }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
+	    if (creep.carry.energy==0) { creep.memory.harvest=true;}
+            if (creep.carry.energy == creep.carryCapacity) { creep.memory.harvest = false;}
+
+	    if(creep.memory.harvest) {
+	            var sources = creep.room.find(FIND_SOURCES_ACTIVE);
+	            if(creep.harvest(creep.pos.findClosestByPath(sources)) == ERR_NOT_IN_RANGE) {
+	                creep.moveTo(creep.pos.findClosestByPath(sources));
+		    }
+	    }
+            else {
+	        var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
                                 structure.structureType == STRUCTURE_TOWER ||
-                                structure.structureType == STRUCTURE_CONTAINER) && structure.energy < structure.energyCapacity ;
+				structure.structureType == STRUCTURE_STORAGE ||
+                                structure.structureType == STRUCTURE_CONTAINER) && (structure.energy < structure.energyCapacity || structure.store < structure.storeCapacity) ;
                     }
-            });
-            if(targets.length > 0) {
+		    });
+		if(targets.length > 0) {
             
                 if(creep.transfer(creep.pos.findClosestByPath(targets), RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.pos.findClosestByPath(targets));
+			creep.moveTo(creep.pos.findClosestByPath(targets));
                 }
-            }
-        }
+		else {creep.memory.harvest=true;}
+                }
+	    }
 	},
         getBodyParts: function() {
-      return [WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE];
-    }
+		return [WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE];
+	}
 };
 
 module.exports = roleHarvester;
